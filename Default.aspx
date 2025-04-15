79c4fff53bde9a888ae21dc3768d52e3d180b6ff
Charles Olavo <venice.charles@fleetbrasil.com.br>
2025-04-15 16:13:29 -0300

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Verificador de Palíndromos</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             Type a number:
+            <asp:TextBox ID="txtInput" runat="server"></asp:TextBox>
+            <asp:Button ID="btnCheck" Text="Verificar" runat="server" OnClick="btnCheck_Click" />
+            <br /><br />
+            <asp:Label ID="lblResult" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.vb
@@ -0,0 +1,29 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            lblResult.Text = String.Empty
+        End If
+    End Sub
+
+    Protected Sub btnCheck_Click(sender As Object, e As EventArgs)
+        Dim number As Integer
+        If Integer.TryParse(txtInput.Text, number) AndAlso number >= 0 Then
+            If IsPalindrome(number) Then
+                lblResult.Text = "It's a palindrome!"
+            Else
+                lblResult.Text = "It is not a palindrome."
+            End If
+        Else
+            lblResult.Text = "Please enter a positive integer."
+        End If
+    End Sub
+
+    Private Function IsPalindrome(number As Integer) As Boolean
+        Dim str As String = $"{number}"
+        Dim reversed As New String(str.Reverse().ToArray())
+        Return str = reversed
+    End Function
+End Class
